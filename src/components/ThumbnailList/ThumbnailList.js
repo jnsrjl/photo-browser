029c857454aa9c488cb89c5e4ad49a1d57d46200
jnsrjl <jnsrjl@gmail.com>
2017-02-28 13:20:27 +0000

    Install axios; Fetch photos from jsonplaceholder; Display thumbnails

--- src/components/ThumbnailList/ThumbnailList.js
+++ src/components/ThumbnailList/ThumbnailList.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import Thumbnail from './Thumbnail/Thumbnail';
 
 class ThumbnailList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/photos?_page=2&_limit=50')
+      .then(res => {
+        this.setState({ photos: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <div>
         <h2>ThumbnailList</h2>
+        { this.renderThumbnails() }
       </div>
     );
+  }
+
+  renderThumbnails() {
+    return this.state.photos.map((photo, id) => (
+      <Thumbnail
+        key={ "thumbnail_" + id }
+        thumbnailUrl={ photo.thumbnailUrl }
+      />
+    ));
   }
 }
 
