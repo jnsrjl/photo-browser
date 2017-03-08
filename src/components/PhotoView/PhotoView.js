26d903bf292abca928a03f505ce6fae0c9cfc587
jnsrjl <jnsrjl@gmail.com>
2017-03-08 21:01:49 +0000

    Fetch and render photo details

--- src/components/PhotoView/PhotoView.js
+++ src/components/PhotoView/PhotoView.js
@@ -5,26 +5,60 @@ class PhotoView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      photo: {}
+      photo: "",
+      album: "",
+      user: ""
     }
   }
 
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/photos/' + this.props.params.id)
+    const baseUrl = 'https://jsonplaceholder.typicode.com/';
+
+    // Fetch photo based on url : photos/:id
+    axios.get(baseUrl + "photos/" + this.props.params.id)
       .then(res => {
-        // Make urls https
+        // Make photo.url https
         res.data.url = res.data.url.substring(0,4) + "s" + res.data.url.substring(4);
         this.setState({ photo: res.data });
+
+        // Photo's albumId for the next fetch
+        return res.data.albumId;
+      })
+      .then(id => axios.get(baseUrl + "albums/" + id))
+      .then(res => {
+        this.setState({ album: res.data })
+
+        // Album's userId for the next fetch
+        return res.data.userId;
+      })
+      .then(id => axios.get(baseUrl + "users/" + id))
+      .then(res => {
+        this.setState({ user: res.data })
       })
       .catch(err => {
-        console.log(err);
-      });
+          if (err.response) {
+            // The request was made, but the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(err.response.data);
+            console.log(err.response.status);
+            console.log(err.response.headers);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log(err.message);
+          }
+          console.log(err.config);
+        });
   }
 
   render() {
     return (
       <div>
         <img src={this.state.photo.url} alt={this.state.photo.title} />
+        <ul>
+          <li>{ this.state.photo.title }</li>
+          <li>by { this.state.user.name }</li>
+          <li>in album { this.state.album.title }</li>
+        </ul>
       </div>
     );
   }
